384683361607e47377bede77e5e02834389ab379
KevinZhu <kevinzhu2468@gmail.com>
2025-07-12 15:25:44 -0400

    Add basic distraction metric using face detection model
    MediaPipe Face Landmark produces a prediction score on if
    a face is detected. We will use this score to determine if
    a user is distracted or not before using more compilated
    scoring metrics.

--- detection_model.py
+++ detection_model.py
@@ -5,11 +5,10 @@ from utils.onnx_utils import create_ort_session
 MP_landmark = create_ort_session("models/mediapipe_face-facelandmarkdetector-w8a8.onnx")
 
 def is_distracted(frame):
-    # dummy return for now. Can trigger by pressing spacebar
-    return int(cv2.waitKey(1) == ord(' '))
-
-    # TODO: use model output to calculate if "distracted"
-    output = get_model_ouptut(frame)
+    score, landmarks = get_model_ouptut(frame)
+    # TODO: implement more complicated distraction detection based on face landmarks
+    # print(score)
+    return int(score > 0.8)
 
 
 def get_model_ouptut(image):
